65a9f79453ba54fd8d4fb0b7fa215eeb6b9701ed
Shereensheer <Shereenahmed330@gmail.com>
2024-10-27 22:06:42 +0500

    assignment7

--- src/app/component/hero/page.tsx
+++ src/app/component/hero/page.tsx
@@ -8,9 +8,7 @@ function Hero() {
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font font-bold sm:text-6xl text-3xl mb-4  text-white">
-      
-      
-      
+    
         Experience  The Best Beauty Services
   </h1>     
         <br className=" text-white  text-sm " />
